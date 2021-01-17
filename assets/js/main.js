c79ba1195ca87ebff410c1812136ac058d519fe1
iTanvir <thisistanvir01@gmail.com>
2021-01-18 03:44:25 +0600

    design the  navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 /*===== SHOW MENU =====*/
+const showMenu = (toggleId, navId)=>{
+   const toggle = document.getElementById(toggleId),
+   nav = document.getElementById(navId)
 
+   if(toggle && nav){
+      toggle.addEventListener('click', ()=>{
+         nav.classList.toggle('show-menu')
+      })
+   }
+}
+showMenu('nav-toggle', 'nav-menu')
 
 /*===== REMOVE MENU MOBILE =====*/
 
