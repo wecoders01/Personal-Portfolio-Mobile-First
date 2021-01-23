c89d384e2e3174bf33b377514dfd929a0ed9ca5d
iTanvir <itanvir@iTanvirs-iMac.Dlink>
2021-01-23 15:48:36 +0600

    Design Home Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,14 +12,48 @@ const showMenu = (toggleId, navId)=>{
 showMenu('nav-toggle', 'nav-menu')
 
 /*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav_link')
 
+function linkAction(){
+   const navMenu = document.getElementById('nav-menu')
+   navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=> n.addEventListener('click', linkAction))
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+   const scrolY = window.pageYOffset
+
+   sections.forEach(current =>{
+      const sectionHeight = current.offsetHeight
+      const sectionTop = current.offsetTop - 50
+      sectionId = current.getAttribute('id')
+
+      if(scrolY > sectionTop && scrolY <= sectionTop + sectionHeight){
+         document.querySelector('.nav_menu a[href*=' + sectionId + ']'.classList.add('active-link'))
+      }else{
+         document.querySelector('.nav_menu a[href*=' + sectionId + ']'.classList.remove('active-link'))
+
+      }
+   })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*===== CHANGE BACKGROUND HEADER =====*/ 
+function scrollHeader(){
+   const header = document.getElementById('header')
+   if(this.scrolY >= 200) header.classList.add('scroll-header');else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
-
+/*===== SHOW SCROLL TOP =====*/ 
+function scrollTop(){
+   const scrollTop = document.getElementById('scroll-top')
+   if(this.scrolY >= 560) scrollTop.classList.add('show-scroll');else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
 /*===== SHOW SCROLL TOP =====*/ 
 
 
